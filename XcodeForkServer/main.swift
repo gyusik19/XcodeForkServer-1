dc1abadb7159b27fc66e713c002c22f15c76b577
arkdusdyk <arkdusdyk@gmail.com>
2021-04-29 21:00:36 +0900

    Add Hello World 1 in team leader server

--- XcodeForkServer/main.swift
+++ XcodeForkServer/main.swift
@@ -3,9 +3,9 @@
 //  XcodeForkServer
 //
 //  Created by YeonYo on 2021/04/29.
+//  Copyright 2020 YeonYo Kim. All rights reserved
 //
 
 import Foundation
 
-print("Hello, World!")
-
+print("Hello World 1 in team leader server")
